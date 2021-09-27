d96ca811ebf030bf25e8dc717e7f00b52ba70a3e
Chris Hunley <cshunley21@gmail.com>
2021-09-27 10:43:37 -0500

    Fixed bonus

--- script.js
+++ script.js
@@ -3,9 +3,9 @@ let randomNum = Math.floor(Math.random() * 5 + 1);
 console.log(randomNum);
 
 // 1b
-if (randomNum) {
+if (randomNum >= 4) {
     console.log("Greater than or equal to 4");
-} else if (randomNum) {
+} else if (randomNum === 2 || 3) {
     console.log("Equal to 2 or 3");
 } else {
     console.log("Equal to 1");
@@ -23,7 +23,7 @@ if (randomNum === 1 || 2 || 4) {
 
 // 1e
 if (randomNum === 2 || 4) {
-    console.log("NOT equal to 3 AND not equal to 5");
+    console.log("NOT equal to 2 AND not equal to 4");
 }
 
 // 2
@@ -31,19 +31,19 @@ randomNum >= 4 ? console.log('Greater than or equal to 4') : console.log('Less t
 
 //3
 switch (randomNum) {
-    case "1" :
+    case 1 :
         console.log("One");
         break;
-    case "2" :
+    case 2 :
         console.log("Two");
         break;
-    case "3" :
+    case 3 :
         console.log("Three");
         break;
-    case "4" :
+    case 4 :
         console.log("Four");
         break;
-    case "5" :
+    case 5 :
         console.log("Five");
         break;
 }
